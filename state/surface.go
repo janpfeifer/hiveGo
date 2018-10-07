fb5554f1ff309eda733cfdc0855302b399dc2948
janpf <pfeifer@gmail.com>
2018-10-07 16:48:24 +0200

    Intermediary work on new ANTs moves algorithm.

--- state/surface.go
+++ state/surface.go
@@ -5,10 +5,68 @@
 // sorrounding the hive. But there can more internal ones.
 package state
 
-// Surface is a connected area of the hive space, where an ANT
+// Surfaces are connected areas of the hive space, where an ANT
 // could presumably move.
-type Surface map[Pos]bool
+type Surfaces struct {
+	PosToSurface map[Pos]int // Maps position to surface it is part of.
+	Num          int         // Number of surfaces.
+}
 
-// Surfaces represent the collection of Surface objects that represent the areas where
-// an ANT can move while connected to a HIVE.
-type Surfaces []Surface
+var UnusedPos = Pos{127, 127}
+
+// Initializes Surfaces from Board position.
+func (b *Board) Surfaces() (surfaces *Surfaces) {
+	surfaces = &Surfaces{}
+
+	for piecePos := range b.board {
+		for pos := range b.EmptyNeighbours(piecePos) {
+			if _, found := surfaces.PosToSurface[pos]; found {
+				// Position already in surfaces.
+				continue
+			}
+
+			// Perform a BFS to find all valid positions.
+			toVisit := map[Pos]bool{pos: true}
+			visited := map[Pos]bool{pos: true}
+			for len(toVisit) > 0 {
+				newToVisit := make(map[Pos]bool)
+				for pos := range toVisit {
+					for _, nextVisit := range b.EmptyAndConnectedNeighbours(pos, UnusedPos, visited) {
+						visited[nextVisit] = true
+						newToVisit[nextVisit] = true
+					}
+				}
+				toVisit = newToVisit
+			}
+
+			// Assign visited positions to new surface.
+			surfaceIndex := surfaces.Num
+			surfaces.Num++
+			for pos := range visited {
+				surfaces.PosToSurface[pos] = surfaceIndex
+			}
+		}
+	}
+	return
+}
+
+func (b *Board) surfaceAntMoves(srcPos Pos, surfaces *Surfaces) (poss []Pos) {
+	visited := map[Pos]bool{srcPos: true}
+	visitedSurfaces := make(map[int]bool, surfaces.Num)
+	for pos := range b.EmptyAndConnectedNeighbours(srcPos, srcPos, visited) {
+		visitedSurfaces[surfaces.PosToSurface[pos]] = true
+	}
+
+	poss = make([]Pos, 0, len(surfaces.PosToSurface))
+	for pos, surfaceNum := range surfaces.PosToSurface {
+		if !visitedSurfaces[surfaceNum] {
+			continue
+		}
+		// TODO: check position is not disconnected: it was only connected to moving
+		//   ANT.
+		if visited[pos] {
+
+		}
+	}
+	return
+}
