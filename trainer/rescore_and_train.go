fb566dc93c627ee0579f41af352e670cc7c3eec0
janpf <pfeifer@gmail.com>
2018-12-24 14:30:40 +0100

    More logging...

--- trainer/rescore_and_train.go
+++ trainer/rescore_and_train.go
@@ -134,6 +134,7 @@ func collectMatchActionsAndIssueLearning(matches []*Match, maInput <-chan MatchA
 			pool[count%poolSize] = ma
 		}
 		count++
+		glog.V(2).Infof("Pool=%d, count=%d", len(pool), count)
 
 		if count > batchSize {
 			if count <= 10*batchSize {
@@ -173,6 +174,7 @@ func continuousLearning(learnInput <-chan LearnParams) {
 	var averageLoss, averageBoardLoss, averageActionsLoss float32
 	count := 0
 	for params := range learnInput {
+		glog.V(2).Infof("Learn: count=%d", count)
 		loss, boardLoss, actionsLoss := players[0].Learner.Learn(
 			params.boards, params.boardLabels,
 			params.actionsLabels, float32(*flag_learningRate),
