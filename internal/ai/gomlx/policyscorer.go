444cb074f6ecfde92c5a4a0eef1217b25afb9ac8
Jan Pfeifer <pfeifer@gmail.com>
2025-04-08 10:44:47 +0200

    Barebones of a gomlx.PolicyScorer -- unfinished.

---
+++ internal/ai/gomlx/policyscorer.go
@@ -0,0 +1,256 @@
+package gomlx
+
+import (
+	"bytes"
+	"fmt"
+	_ "github.com/gomlx/gomlx/backends/xla"
+	"github.com/gomlx/gomlx/graph"
+	"github.com/gomlx/gomlx/ml/context"
+	"github.com/gomlx/gomlx/ml/context/checkpoints"
+	"github.com/gomlx/gomlx/ml/train"
+	"github.com/gomlx/gomlx/ml/train/optimizers"
+	"github.com/gomlx/gomlx/types/tensors"
+	"github.com/janpfeifer/hiveGo/internal/ai"
+	"github.com/janpfeifer/hiveGo/internal/generics"
+	"github.com/janpfeifer/hiveGo/internal/parameters"
+	"github.com/janpfeifer/hiveGo/internal/state"
+	"github.com/pkg/errors"
+	"k8s.io/klog/v2"
+	"slices"
+	"sync"
+)
+
+// newPolicyScorer returns a gomlx.PolicyScorer for the given ValueModel.
+func newPolicyScorer(modelType ModelType, filePath string, model PolicyModel, params parameters.Params) (*PolicyScorer, error) {
+	s := &PolicyScorer{
+		Type:  modelType,
+		model: model,
+	}
+
+	// Help if requested.
+	if slices.Index([]string{"help", "--help", "-help", "-h"}, filePath) != -1 {
+		s.writeHyperparametersHelp()
+		return nil, fmt.Errorf("model type %s help requested", modelType)
+	}
+
+	// Number of checkpoints to keep.
+	var err error
+	s.checkpointsToKeep, err = parameters.PopParamOr(params, "keep", 10)
+	if err != nil {
+		return nil, err
+	}
+
+	// Create checkpoint, and load it if it exists.
+	if filePath != "" {
+		if err = s.createCheckpoint(filePath); err != nil {
+			return nil, errors.WithMessagef(err, "failed to build checkpoint for model %s in path %s",
+				modelType, filePath)
+		}
+	}
+
+	// Create the backend.
+	_ = backend()
+
+	// Overwrite hyperparameters from given params.
+	err = extractParams(s.Type.String(), params, s.model.Context())
+	if err != nil {
+		return nil, err
+	}
+	ctx := s.model.Context()
+	s.batchSize = context.GetParamOr(ctx, "batch_size", 100)
+
+	// Create optimizer to be used in training.
+	s.optimizer = optimizers.FromContext(ctx)
+
+	// Setup scoreExec executor.
+	muNewClient.Lock()
+	defer muNewClient.Unlock()
+	s.scoreExec = context.NewExec(backend(), ctx,
+		func(ctx *context.Context, inputs []*graph.Node) *graph.Node {
+			// Remove last axis with dimension 1.
+			ctx = ctx.Checked(false)
+			return graph.Squeeze(s.model.ForwardGraph(ctx, inputs), -1)
+		})
+	s.lossExec = context.NewExec(backend(), ctx,
+		func(ctx *context.Context, inputsAndLabels []*graph.Node) *graph.Node {
+			inputs := inputsAndLabels[:len(inputsAndLabels)-1]
+			labels := inputsAndLabels[len(inputsAndLabels)-1]
+			if labels.Rank() == 1 {
+				// Add the last axes with dimension 1.
+				labels = graph.ExpandAxes(labels, -1)
+			}
+			loss := s.model.LossGraph(ctx, inputs, labels)
+			if !loss.IsScalar() {
+				// Some losses may return one value per example of the batch.
+				loss = graph.ReduceAllMean(loss)
+			}
+			return loss
+		})
+	s.trainStepExec = context.NewExec(backend(), s.model.Context(),
+		func(ctx *context.Context, inputsAndLabels []*graph.Node) *graph.Node {
+			inputs := inputsAndLabels[:len(inputsAndLabels)-1]
+			labels := inputsAndLabels[len(inputsAndLabels)-1]
+			g := labels.Graph()
+			ctx.SetTraining(g, true)
+			loss := s.model.LossGraph(ctx, inputs, labels)
+			s.optimizer.UpdateGraph(ctx, g, loss)
+			train.ExecPerStepUpdateGraphFn(ctx, g)
+			return loss
+		})
+
+	// Force creating/loading of variables without race conditions first.
+	board := state.NewBoard()
+	_ = s.Score(board)
+
+	return s, nil
+}
+
+// PolicyScorer implements a generic GoMLX "board scorer" (to use with AlphaBetaPruning or MinMax seachers) for the Hive game.
+// It only models the estimate of the state value (Q).
+//
+// It implements ai.PolicyScorer, ai.BatchPolicyScorer and ai.ValueLearner.
+//
+// It is just a wrapper around on of the models implemented.
+type PolicyScorer struct {
+	Type ModelType
+
+	// model if PolicyScorer is a a PolicyScorer.
+	model PolicyModel
+
+	// Executors.
+	valueScoreExec, policyScoreExec, lossExec, trainStepExec *context.Exec
+
+	// checkpoint handler, if model is being saved/loaded to/from disk.
+	checkpoint *checkpoints.Handler
+
+	// checkpointsToKeep is the number of copies of older checkpoints to keep around.
+	// Default to 10.
+	checkpointsToKeep int
+
+	// Hyperparameters cached values: they should also be set in modelCtx.
+	batchSize int
+
+	// muLearning "write" for learning, and "read" for scoring.
+	muLearning sync.RWMutex
+
+	// optimizer used when training the model.
+	// ?Should this be owned by the model itself?
+	optimizer optimizers.Interface
+
+	// muSave makes saving sequential.
+	muSave sync.Mutex
+}
+
+var (
+	// Assert PolicyScorer is an ai.PolicyScorer, an ai.BatchPolicyScorer and an ai.ValueLearner.
+	_ ai.PolicyScorer = (*PolicyScorer)(nil)
+	_ ai.ValueLearner = (*PolicyScorer)(nil)
+)
+
+// String implements fmt.Stringer and ai.PolicyScorer.
+func (s *PolicyScorer) String() string {
+	if s == nil {
+		return "<nil>[GoMLX]"
+	}
+	if s.checkpoint == nil {
+		return fmt.Sprintf("%s[GoMLX]", s.Type)
+	}
+	return fmt.Sprintf("%s[GoMLX]@%s", s.Type, s.checkpoint.Dir())
+}
+
+// BoardScore implements ai.PolicyScorer (which includes ai.ValueScorer).
+func (s *PolicyScorer) Score(board *state.Board) float32 {
+	inputs := s.model.CreateValueInputs(board)
+	s.muLearning.RLock()
+	defer s.muLearning.RUnlock()
+	donatedInputs := generics.SliceMap(inputs, func(t *tensors.Tensor) any {
+		return graph.DonateTensorBuffer(t, backend())
+	})
+
+	scoreT := s.valueScoreExec.Call(donatedInputs...)[0]
+	return tensors.ToScalar[float32](scoreT)
+}
+
+// BatchBoardScore implements ai.BatchPolicyScorer.
+func (s *PolicyScorer) BatchScore(boards []*state.Board) []float32 {
+	return generics.SliceMap(boards, func(board *state.Board) float32 {
+		return s.Score(board)
+	})
+}
+
+// PolicyScore implements ai.PolicyScorer.
+func (s *PolicyScorer) PolicyScore(board *state.Board) []float32 {
+	inputs := s.model.CreatePolicyInputs(board)
+	s.muLearning.RLock()
+	defer s.muLearning.RUnlock()
+	donatedInputs := generics.SliceMap(inputs, func(t *tensors.Tensor) any {
+		return graph.DonateTensorBuffer(t, backend())
+	})
+	scoreT := s.policyScoreExec.Call(donatedInputs...)[0]
+	return tensors.CopyFlatData[float32](scoreT)
+}
+
+// Learn implements ai.ValueLearner, and trains model with the new boards and its labels.
+// It returns the lossExec.
+func (s *PolicyScorer) Learn(boards []*state.Board, boardLabels []float32) (loss float32) {
+	//fmt.Printf("Learn(%d boards)\n", len(boards))
+	s.muLearning.Lock()
+	defer s.muLearning.Unlock()
+	lossT := s.trainStepExec.Call(s.createInputsAndLabels(boards, boardLabels)...)[0]
+	return tensors.ToScalar[float32](lossT)
+}
+
+// Loss returns a measure of lossExec for the model -- whatever it is.
+func (s *PolicyScorer) Loss(boards []*state.Board, boardLabels []float32) (loss float32) {
+	s.muLearning.RLock()
+	defer s.muLearning.RUnlock()
+	lossT := s.lossExec.Call(s.createInputsAndLabels(boards, boardLabels)...)[0]
+	return tensors.ToScalar[float32](lossT)
+}
+
+func (s *PolicyScorer) createInputsAndLabels(boards []*state.Board, boardLabels []float32) []any {
+	inputs := s.model.CreateInputs(boards)
+	inputs = append(inputs, s.model.CreateLabels(boardLabels))
+	donatedInputs := generics.SliceMap(inputs, func(t *tensors.Tensor) any {
+		return graph.DonateTensorBuffer(t, backend())
+	})
+	return donatedInputs
+}
+
+// Save should save the model.
+func (s *PolicyScorer) Save() error {
+	if s.checkpoint == nil {
+		klog.Warningf("This %s model is not associated to a checkpoint directory,  not saving", s.Type)
+		return nil
+	}
+	return s.checkpoint.Save()
+}
+
+// BatchSize returns the recommended batch size and implements ai.ValueLearner.
+func (s *PolicyScorer) BatchSize() int {
+	return s.batchSize
+}
+
+// writeHyperparametersHelp enumerates all the hyperparameters set in the context.
+func (s *PolicyScorer) writeHyperparametersHelp() {
+	buf := &bytes.Buffer{}
+	_, _ = fmt.Fprintf(buf, "ValueModel %s parameters:\n", s.Type)
+	s.model.Context().EnumerateParams(func(scope, key string, value any) {
+		if scope != context.RootScope {
+			return
+		}
+		_, _ = fmt.Fprintf(buf, "\t%q: default value is %v\n", key, value)
+	})
+	klog.Info(buf)
+}
+
+func (s *PolicyScorer) createCheckpoint(filePath string) error {
+	var err error
+	s.checkpoint, err = checkpoints.
+		Build(s.model.Context()).
+		Dir(filePath).
+		Immediate().
+		Keep(10).
+		Done()
+	return err
+}
