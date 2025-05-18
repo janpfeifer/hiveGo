5257433338cd858643b7c7f20449afa306020bfb
Jan Pfeifer <pfeifer@gmail.com>
2025-05-18 11:08:41 +0200

    Fixed time format for a0-trainer.

--- cmd/a0-trainer/main.go
+++ cmd/a0-trainer/main.go
@@ -67,7 +67,7 @@ func main() {
 	}
 	var currentExamples []Example
 	for i := 0; *flagNumIterations <= 0 || i < *flagNumIterations; i++ {
-		fmt.Printf("\nIteration #%d - %s\n", i, time.Now().Format("2006/01/02 15:04:05"))
+		fmt.Printf("\nIteration #%d - %s\n", i, time.Now().Format("2006-01-02 15:04:05"))
 		var newExamples []Example
 		if bootstrapAiPlayer != nil {
 			_, _, _, newExamples = must.M4(runMatches(globalCtx, *flagNumMatches, aiPlayer, bootstrapAiPlayer))
